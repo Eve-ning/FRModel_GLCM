5c116b23b3b9d0ca616b228f7ccea5c1dd243a44
Evening <johnchangqi@hotmail.com>
2022-05-31 18:16:39 +0800

    Add INFO_CROSS.md pathing

--- src/conf.py
+++ src/conf.py
@@ -8,4 +8,5 @@ OUTPUT_DIR_GLCM_CROSS = DATA_DIR / "glcm_cross"
 
 KEY_PATH = PROJ_DIR / "key/frmodel-4e6c3ce6ea28.json"
 INFO_MD_FILE = DATA_DIR / "INFO.md"
+INFO_CROSS_MD_FILE = DATA_DIR / "INFO_CROSS.md"
 BOUNDS_FILE = "bounds.csv"
